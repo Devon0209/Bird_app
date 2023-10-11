d4a210f3c1e06c70c95bb5fdfc622ae77628bbbb
Tyrese <Devon0209@gmail.com>
2023-10-11 10:20:22 +0200

    added simple current location marker

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gomaps_btn: Button = findViewById(R.id.gomaps_btn)
+        gomaps_btn.setOnClickListener {
+            // Create an Intent to open the destination Activity
+            val intent = Intent (this@MainActivity, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
