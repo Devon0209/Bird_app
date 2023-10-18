f1aebe02aca3d498cba17d5c6f5d05c1ae007a33
TheKey213 <azdilinga@gmail.com>
2023-10-18 17:17:23 +0000

    new files: Hotspots are now working

--- app/src/main/java/com/example/myapplication/LocationFragment.kt
+++ app/src/main/java/com/example/myapplication/LocationFragment.kt
@@ -3,57 +3,45 @@ package com.example.myapplication
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.*
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONException
+import java.io.IOException
+import kotlin.concurrent.thread
 
-class LocationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
-
-
-
-    val client: OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor { chain ->
-            val newRequest = chain.request().newBuilder()
-                .addHeader("X-eBirdApiToken", "tsmqqtr4glk1")
-                .build()
-            chain.proceed(newRequest)
-        }
-        .build()
-
-    private val api = Retrofit.Builder()
-        .baseUrl("https://api.ebird.org/v2/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(EBirdApiService::class.java)
+class LocationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener,
+    GoogleApiClient.OnConnectionFailedListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-
     private lateinit var currentLocationButton: Button
-    private lateinit var locationNameTextView: TextView
-    private lateinit var latitudeTextView: TextView
-    private lateinit var longitudeTextView: TextView
 
     // Custom marker for hotspots
     private lateinit var hotspotMarker: BitmapDescriptor
@@ -69,12 +57,9 @@ class LocationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClick
         val rootView = inflater.inflate(R.layout.fragment_location, container, false)
 
         currentLocationButton = rootView.findViewById(R.id.currentLocationButton)
-        locationNameTextView = rootView.findViewById(R.id.locationNameTextView)
-        latitudeTextView = rootView.findViewById(R.id.latitudeTextView)
-        longitudeTextView = rootView.findViewById(R.id.longitudeTextView)
 
         // Load a custom marker image for hotspots
-        hotspotMarker = BitmapDescriptorFactory.fromResource(R.drawable.hotspot_marker)
+        hotspotMarker = createCustomMarker(Color.argb(100, 0, 0, 255))
 
         currentLocationButton.setOnClickListener {
             // Get the user's current location and add a marker to the map
@@ -145,23 +130,92 @@ class LocationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClick
     }
 
     private fun getNearbyHotspots(latitude: Double, longitude: Double) {
-        GlobalScope.launch(Dispatchers.IO) {
-            val hotspots = api.getNearbyHotspots(20, latitude, longitude, 30, "json")
-            activity?.runOnUiThread {
-                // Handle the list of nearby hotspots
-                if (hotspots != null) {
-                    for (hotspot in hotspots) {
-                        // Add markers for nearby hotspots on the map with custom marker
-                        val hotspotLatLng = LatLng(hotspot.latitude, hotspot.longitude)
-                        val markerOptions = MarkerOptions()
-                            .position(hotspotLatLng)
-                            .title(hotspot.name)
-                            .icon(hotspotMarker) // Use the custom marker
-                        mMap.addMarker(markerOptions)
+        val apiKey = "tsmqqtr4glk1"
+
+        // Build the URL for the eBird API
+        val url =
+            "https://api.ebird.org/v2/ref/hotspot/geo?lat=$latitude&lng=$longitude&dist=50&fmt=json"
+
+        thread {
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .url(url)
+                    .addHeader("X-eBirdApiToken", apiKey)
+                    .build()
+
+                val response = client.newCall(request).execute()
+                if (response.isSuccessful) {
+                    val responseData = response.body()?.string()
+                    responseData?.let {
+                        parseHotspotData(it)
                     }
+                } else {
+                    activity?.runOnUiThread {
+                        Toast.makeText(
+                            requireContext(),
+                            "Failed to fetch nearby hotspots",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                activity?.runOnUiThread {
+                    Toast.makeText(requireContext(), "Error: ${e.message}", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
+
+
+    private fun parseHotspotData(responseData: String?) {
+        responseData?.let {
+            try {
+                val hotspotsArray = JSONArray(it)
+                for (i in 0 until hotspotsArray.length()) {
+                    val hotspot = hotspotsArray.getJSONObject(i)
+                    val name = hotspot.getString("locName")
+                    val lat = hotspot.getDouble("lat")
+                    val lng = hotspot.getDouble("lng")
+                    val hotspotLocation = LatLng(lat, lng)
+
+                    activity?.runOnUiThread {
+                        mMap.addMarker(
+                            MarkerOptions()
+                                .position(hotspotLocation)
+                                .title(name)
+                                .icon(hotspotMarker)
+                        )
+                    }
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+                activity?.runOnUiThread {
+                    Toast.makeText(
+                        requireContext(),
+                        "Error parsing hotspot data: ${e.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun createCustomMarker(color: Int): BitmapDescriptor {
+        val width = 48
+        val height = 48
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(color)
+
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.d("LocationFragment", "Connection failed: $connectionResult")
+    }
 }
+
+
 
